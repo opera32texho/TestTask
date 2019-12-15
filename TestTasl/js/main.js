8585fa389b53f38dcd76617a2c1ccbd5cb317c3c
Vitalii <wcrwcr1993@gmail.com>
2019-12-16 00:03:27 +0200

    80%Create

--- TestTasl/js/main.js
+++ TestTasl/js/main.js
@@ -1,6 +1,25 @@
-$("#submit-modal-window").click(function(){
-      
-    $("#list-home-list").before( '<a href="#task-name">' + text + '</a>');  
-        
+$("#submit-modal-window").click(function () {
+    let text = $("#task-name").val();
+    let textDescribe = $("#describe-name").val();
+  
+    function getNewTabId() {
+      // Генерим рандомный айдишник
+      let randomId = 'task' + getRandomInt(1000000000);
+      // проверяем - не сущесвтует ли уже элемент с таким ID
+      let exists = $('#' + randomId).length !== 0;
+  
+      // если элемент с таким ID уже существует - генерируем новый
+      return exists ? getNewTabId() : randomId;
+    }
+  
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+  
+    let tabId = getNewTabId();
+    
+    $("#list-tab").prepend('<a class="list-group-item list-group-item-action " href="#' + tabId + '" data-toggle="list" role="tab" aria-controls="task">' + text + '</a>'); 
+    $("#nav-tabContent").prepend('<div class="tab-pane fade show " id="' + tabId + '">' + textDescribe + '</div>');
+    $('#exampleModal').modal('hide');
   });
-
+ 
