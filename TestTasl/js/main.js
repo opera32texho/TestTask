6ed32644d32c4c59a756ac8c67ffc5bc1f0b5728
Vitalii <wcrwcr1993@gmail.com>
2019-12-18 02:30:50 +0200

    End project with bugs(edit all task)

--- TestTasl/js/main.js
+++ TestTasl/js/main.js
@@ -33,12 +33,20 @@ $("#submit-modal-window").click(function () {
     
     $("#list-tab").prepend('<a class="list-group-item list-group-item-action " href="#' + tabId + '" data-toggle="list" role="tab" aria-controls="task">' + text + '</a>'); 
     $("#nav-tabContent").prepend('<div class="tab-pane fade show " id="' + tabId + '">' + textDescribe + '</div>');
-    $("#holder").prepend('<div class="custom-control custom-checkbox mb-3 changeedit"><input type="checkbox" class="custom-control-input" id="' + tabIdForCheckbox + '" required><label class="custom-control-label" for="' + tabIdForCheckbox + '"></label></div></form>' );
-    $("#holder").prepend('<button type="button" class="btn btn-primary change-button" data-toggle="modal" data-target="#exampleModalCenter">Edit</button><div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true"><div class="modal-dialog modal-dialog-centered" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="exampleModalLongTitle">Change your task</h5><button type="button" class="close" data-dismiss="modal" aria-label="Clos<span aria-hidden="true">&times;</span></button></div><div class="modal-body"></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button><button type="button" class="btn btn-primary">Save changes</button></div></div></div></div>' );
-    
+    $("#holder").append('<div>' + '<div class="custom-control custom-checkbox mb-3 changeedit"><input type="checkbox" class="custom-control-input" id="' + tabIdForCheckbox + '" required><label class="custom-control-label" for="' + tabIdForCheckbox + '"></label></div></form> ' +'<button type="button" class="btn btn-primary change-button" data-toggle="modal" data-target="#exampleModalCenter">Edit</button>' +'</div>');
     $('#exampleModal').modal('hide');
-
+   
 });
+  $('#exampleModalCenter').on('show.bs.modal', function (e) {
+    let idx = $(e.relatedTarget).parent().index();
+    let taskName = $('#list-tab > a:nth-child(' + (idx + 1) + ')').text();
+    $('#exampleModalCenter .task-name').val(taskName);
+    $('#save-task-change').click(function(){
+  
+      let text = $('.task-name').val();
 
+      $('#list-tab > a:nth-child(' + (idx + 1) + ')').text(text);
+      $('#exampleModalCenter').modal('hide');
+    });
+  });
 
-let taskCount = $('.list-group-item a').length;
